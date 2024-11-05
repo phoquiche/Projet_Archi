ae28c3da00a28578f2edf554f1f5000c5f45468d
phoquiche <dustybertin@gmail.com>
2024-11-05 17:21:18 +0100

    add everything

---
+++ src/main/java/com/example/projet_archilog/ProjetArchilogApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_archilog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetArchilogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetArchilogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projet_archilog/ProjetArchilogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projet_archilog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetArchilogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
